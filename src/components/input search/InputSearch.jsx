3d77c2c29d84d2053fb811cf6679f9dac084c12c
Matias Arias <matt.arias182@gmail.com>
2022-09-15 04:43:37 -0300

    finalizando input search para buscar por ciudad

--- src/components/input search/InputSearch.jsx
+++ src/components/input search/InputSearch.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { BsSearch } from "react-icons/bs";
 
-const InputSearch = () => {
-  return <div>InputSearch</div>;
+const InputSearch = ({ inputCity, setInputCity }) => {
+  const handleInputSearch = ({ target }) => {
+    setInputCity(target.value);
+    // console.log(target.value);
+  };
+
+  return (
+    <>
+      <form className="flex items-center">
+        <input
+          className="relative w-[300px] h-12 px-4 bg-gray-600/70 border-y-4 border-l-4 border-gray-200/70 rounded-tl-lg rounded-bl-lg focus:outline-none placeholder:italic placeholder:text-gray-100 text-gray-100"
+          type="text"
+          placeholder="Enter Location"
+          maxLength="40"
+          value={inputCity}
+          onChange={handleInputSearch}
+        />
+        <button
+          type="submit"
+          className="w-12 h-12 rounded-tr-lg rounded-br-lg border-y-4 border-r-4 border-l-2 border-gray-200/70 bg-gray-600/70 flex justify-center items-center"
+        >
+          <BsSearch className="text-2xl text-gray-100 animate-pulse" />
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default InputSearch;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,68 @@
 import React, { useEffect, useState } from "react";
+import InputSearch from "../components/input search/InputSearch";
 
 const Home = () => {
   const [unsplashImg, setUnsplashImg] = useState(null);
 
   const [currentWeather, setCurrentWeather] = useState([]);
 
-  // const currentWeatherApi = async () => {
+  const [inputCity, setInputCity] = useState("buenos aires");
+
+  const currentWeatherApi = async (city) => {
+    try {
+      const resp = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_MAP_API_KEY}&units=metric`
+      );
+      const data = await resp.json();
+      console.log(data);
+      setCurrentWeather(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    currentWeatherApi(inputCity);
+  }, [inputCity]);
+
+  // const handleSubmitCity = (e) => {
+  //   e.preventDefault();
+  //   currentWeatherApi(inputCity);
+  //   setInputCity("");
+  // };
+
+  // const unsplashApi = async () => {
   //   try {
-  //     const resp = await fetch(
-  //       `https://api.openweathermap.org/data/2.5/weather?q=mendoza&appid=fb9e237ca55ec441e335dd925bae5de2&units=metric`
+  //     const response = await fetch(
+  //       `https://api.unsplash.com/photos/random/?client_id=j09bjTjlzT-9ENnmmxvYs-ljixACpN8mQhwgqkBvN9c&orientation=landscape&query=nature&count=1`
   //     );
-  //     const data = await resp.json();
-  //     console.log(data);
-  //     setCurrentWeather(data);
+  //     const dataImg = await response.json();
+  //     console.log(dataImg);
+  //     setUnsplashImg(dataImg[0].urls.full);
   //   } catch (error) {
   //     console.log(error);
   //   }
   // };
 
   // useEffect(() => {
-  //   currentWeatherApi();
+  //   unsplashApi();
   // }, []);
 
-  const unsplashApi = async () => {
-    const response = await fetch(
-      `https://api.unsplash.com/photos/random/?client_id=j09bjTjlzT-9ENnmmxvYs-ljixACpN8mQhwgqkBvN9c&orientation=landscape&query=nature&count=1`
-    );
-    const dataImg = await response.json();
-    console.log(dataImg);
-    setUnsplashImg(dataImg[0].urls.full);
-  };
-
-  useEffect(() => {
-    unsplashApi();
-  }, []);
-
   return (
-    <div className="relative h-screen w-full flex flex-col justify-center items-center">
-      <img
+    <div className="relative h-screen w-full">
+      {/* <img
         className="h-full w-full object-cover"
         src={unsplashImg}
         alt="unsplash nature"
-      />
-      <div className="absolute top-0 left-0 h-full w-full bg-slate-800/50"></div>
-
-      {/* <div>
-        <form>
-          <input
-            className="w-[300px] px-4 py-2 border-2 border-green-800 rounded-lg focus:outline-none focus:border-lime-500 placeholder:italic placeholder:text-slate-500"
-            type="text"
-            placeholder="Enter Location"
-            maxLength="40"
-          />
-        </form>
-      </div> */}
+      /> */}
+      <div className="absolute top-0 left-0 h-full w-full bg-gray-800/30"></div>
+      <div className="absolute top-0 left-0 h-full w-full flex flex-col justify-around items-center">
+        <InputSearch
+          inputCity={inputCity}
+          setInputCity={setInputCity}
+          // handleSubmitCity={handleSubmitCity}
+        />
+      </div>
     </div>
   );
 };
